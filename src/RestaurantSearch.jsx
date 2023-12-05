6ee129bf953be2ecab166cf020f867dccc0aeec8
jamescorriveau <137570538+jamescorriveau@users.noreply.github.com>
2023-12-05 10:48:06 -0500

    adding search, header, css

---
+++ src/RestaurantSearch.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import "./App.css";
+
+const RestaurantSearch = ({ restaurants }) => {
+  const [searchInput, setSearchInput] = useState("");
+  const [filteredRestaurants, setFilteredRestaurants] = useState(restaurants);
+
+  const handleChange = (e) => {
+    setSearchInput(e.target.value);
+  };
+
+  const handleSearch = () => {
+    const filtered =
+      searchInput.length > 0
+        ? restaurants.filter((restaurant) =>
+            restaurant.name.toLowerCase().includes(searchInput.toLowerCase())
+          )
+        : restaurants;
+
+    setFilteredRestaurants(filtered);
+  };
+
+  return (
+    <div>
+      <input
+        type="search"
+        placeholder="Search here"
+        onChange={handleChange}
+        value={searchInput}
+        className="search-restaurant"
+      />
+      <button onClick={handleSearch}>Search</button>
+
+      <table>
+        <thead>
+          <tr></tr>
+        </thead>
+        <tbody>
+          {filteredRestaurants.map((restaurant, index) => (
+            <tr key={index}>
+              <td>{restaurant.name}</td>
+              <td>{restaurant.location}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default RestaurantSearch;
